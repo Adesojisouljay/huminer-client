c0427eedcffdd79eb77ad84eb78748db23362cd7
Adesojisouljay <adesojiisrael@gmail.com>
2026-02-20 00:45:17 +0100

    refactor: remove `REACT_APP_HUMINER_API` environment variable from base URL determination.

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const PROD_URL = "https://devapi.huminer.com/api";
 const DEV_URL = "http://localhost:2111/api";
 
-const BASE_URL = process.env.REACT_APP_HUMINER_API || (process.env.NODE_ENV === "production" ? PROD_URL : DEV_URL);
+const BASE_URL = process.env.NODE_ENV === "production" ? PROD_URL : DEV_URL;
 
 export const API = axios.create({
   baseURL: BASE_URL,
